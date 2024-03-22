7ebaf961b9afee3a2f50c739227433a656f0c0c6
Luke Richardson-Foulger <87760589+EkulRF@users.noreply.github.com>
2024-03-22 12:02:06 +0000

    Working data download script

--- Download_Data.py
+++ Download_Data.py
@@ -1,7 +1,13 @@
 import requests
 
-the_url = 'https://liveuclac-my.sharepoint.com/:f:/g/personal/ucfalri_ucl_ac_uk/Eo1MgmImegpDmlRtRjfLTc4BQUfw73dds0ha8Qk5u4kPZw?download=1'
-response = requests.get(the_url)
+sc_file = 'https://liveuclac-my.sharepoint.com/:u:/r/personal/ucfalri_ucl_ac_uk/Documents/Work/8_TELOPS/Test_Data/2023_TestFlightData/20230612_Flight/WhittlesleyAOI/raw/Mosaic.radiance.sc?download=1'
+hdr_file = 'https://liveuclac-my.sharepoint.com/:u:/r/personal/ucfalri_ucl_ac_uk/Documents/Work/8_TELOPS/Test_Data/2023_TestFlightData/20230612_Flight/WhittlesleyAOI/raw/Mosaic.radiance.hdr?download=1'
 
-with open('Test_Data/test_me.pdf', 'wb') as fp:
-    fp.write(response.content)
+sc_file = requests.get(sc_file)
+hdr_file = requests.get(hdr_file)
+
+with open('Test_Data/Mosaic.radiance.sc', 'wb') as fp:
+    fp.write(sc_file.content)
+
+with open('Test_Data/Mosaic.radiance.hdr', 'wb') as fp:
+    fp.write(hdr_file.content)
